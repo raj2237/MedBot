e4b1a36ddff3ef2337f570e1e165ffbd47b3c74c
raj2237 <rajveershah7111@gmail.com>
2025-03-15 10:05:14 +0530

    Folder structure done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages , setup
+
+setup(
+    name = "Generative AI Project",
+    version = '0.0.0',
+    author = "Raj Shah",
+    author_email = "rajveershah7111@gmail.com",
+    packages = find_packages(),
+    install_requires = [],
+)
